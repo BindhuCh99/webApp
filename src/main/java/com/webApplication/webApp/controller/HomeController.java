c2ccadf849912aea37dcf77023e3f199c1e788ee
BindhuCh99 <113169515+BindhuCh99@users.noreply.github.com>
2024-05-02 17:23:58 -0500

    get method with params auto converted to object

---
+++ src/main/java/com/webApplication/webApp/controller/HomeController.java
@@ -0,0 +1,23 @@
+package com.webApplication.webApp.controller;
+
+
+import com.webApplication.webApp.StudentRepo;
+import com.webApplication.webApp.Student;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+	@Autowired
+	StudentRepo repo;
+	@RequestMapping("/home")
+	public String Home() {
+		return "home";
+	}
+	@RequestMapping("/addStudent")
+	public String addStudent( Student student){
+		repo.save(student);
+		return "home";
+	}
+}
