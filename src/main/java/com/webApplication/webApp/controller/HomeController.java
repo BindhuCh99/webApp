5e626c0d97b3b82647e4c06bf08c45d57b261c6a
BindhuCh99 <113169515+BindhuCh99@users.noreply.github.com>
2024-05-06 15:57:03 -0500

    fetching data and printing it

--- src/main/java/com/webApplication/webApp/controller/HomeController.java
+++ src/main/java/com/webApplication/webApp/controller/HomeController.java
@@ -6,18 +6,34 @@ import com.webApplication.webApp.Student;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.Optional;
 
 @Controller
 public class HomeController {
 	@Autowired
 	StudentRepo repo;
-	@RequestMapping("/home")
-	public String Home() {
-		return "home";
+	@RequestMapping("/studentForm")
+	public String studentForm() {
+		return "studentForm";
+	}
+	@RequestMapping("/searchStudentInfo")
+	public String searchStudentInfo() {
+		return "searchStudentInfo";
 	}
 	@RequestMapping("/addStudent")
 	public String addStudent( Student student){
 		repo.save(student);
-		return "home";
+		return "studentForm";
+	}
+	@RequestMapping("/getStudent")
+	public ModelAndView getStudent(@RequestParam int id){
+		ModelAndView mv=new ModelAndView();
+		Student student=repo.findById(id).orElse(new Student());
+		mv.addObject("student", student);
+		mv.setViewName("studentInfo");
+		return mv;
 	}
 }
