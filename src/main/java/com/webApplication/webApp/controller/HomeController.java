493e8da30b6fbc2332ee65ba0ddd32ce8a50bb4b
BindhuCh99 <113169515+BindhuCh99@users.noreply.github.com>
2024-05-07 12:00:15 -0500

    Final version Using Rest Controller

--- src/main/java/com/webApplication/webApp/controller/HomeController.java
+++ src/main/java/com/webApplication/webApp/controller/HomeController.java
@@ -5,35 +5,58 @@ import com.webApplication.webApp.StudentRepo;
 import com.webApplication.webApp.Student;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.util.List;
 import java.util.Optional;
 
-@Controller
+@RestController
 public class HomeController {
 	@Autowired
 	StudentRepo repo;
-	@RequestMapping("/studentForm")
+	@GetMapping("/studentForm")
 	public String studentForm() {
 		return "studentForm";
 	}
-	@RequestMapping("/searchStudentInfo")
+	@GetMapping("/searchStudentInfo")
 	public String searchStudentInfo() {
 		return "searchStudentInfo";
 	}
-	@RequestMapping("/addStudent")
-	public String addStudent( Student student){
+
+	@GetMapping("/getStudents")
+	public List<Student> getStudents(){
+		return repo.findAll();
+
+	}
+	@GetMapping("/getStudent/{id}")
+	public Optional<Student> getStudentById(@PathVariable int id){
+		return repo.findById(id);
+	}
+	@PostMapping("/addStudent")
+	public Student addStudent(@RequestBody Student student){
 		repo.save(student);
-		return "studentForm";
+		return student;
 	}
-	@RequestMapping("/getStudent")
-	public ModelAndView getStudent(@RequestParam int id){
-		ModelAndView mv=new ModelAndView();
-		Student student=repo.findById(id).orElse(new Student());
-		mv.addObject("student", student);
-		mv.setViewName("studentInfo");
-		return mv;
+	@PutMapping("/updateStudent")
+	public Student updateStudent(@RequestBody Student student){
+		repo.save(student);
+		return student;
 	}
+	@DeleteMapping("/deleteStudent/{id}")
+	public String deleteStudent(@PathVariable int id){
+		Student student=repo.findById(id).orElse(null);
+		repo.delete(student);
+		return "Deleted";
+	}
+
+//	@GetMapping("/getStudent")
+//	public ModelAndView getStudent(@RequestParam int id){
+//		ModelAndView mv=new ModelAndView();
+//		Student student=repo.findById(id).orElse(new Student());
+//		mv.addObject("student", student);
+//		mv.setViewName("studentInfo");
+//		return mv;
+//	}
+
 }
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,1 +1,6 @@
 insert into student values(1,'bindu','java');
+insert into student values(2,'harsha','AI');
+insert into student values(3,'vani','java');
+insert into student values(4,'sresta','CP');
+insert into student values(5,'rakesh','ML');
+
